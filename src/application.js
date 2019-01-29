d4edc4d872bf9ae6fd7affac809db540787f9c88
NimfaMargo <Nikiforova.Rita@gmail.com>
2019-01-29 13:42:01 +0300

    remove renders logic

--- src/application.js
+++ src/application.js
@@ -1,6 +1,7 @@
 import WatchJS from 'melanke-watchjs';
 import isURL from 'validator/lib/isURL';
 import axios from 'axios';
+import { renderItemsList, renderChanelList } from './renders';
 
 export default () => {
   const parser = new DOMParser();
@@ -12,7 +13,7 @@ export default () => {
     },
     urllist: [],
   };
-  const body = document.querySelector('body');
+
   const button = document.querySelector('button.btn-outline-success');
   const inputForm = document.querySelector('input.form-control');
 
@@ -24,37 +25,6 @@ export default () => {
       inputForm.style.border = 'thick solid red';
     }
   });
-
-  const makeItemsList = (objOfItems) => {
-    const items = Object.values(objOfItems);
-    const list = document.createElement('ul');
-    list.classList.add('list-group', 'list-group-flush');
-    items.forEach((item) => {
-      const listItem = document.createElement('li');
-      listItem.classList.add('list-group-item');
-      const anchor = document.createElement('a');
-      anchor.href = item.querySelector('link');
-      anchor.innerText = item.querySelector('title').textContent;
-      listItem.append(anchor);
-      list.append(listItem);
-    });
-    body.append(list);
-  };
-  const makeFeedList = (title, description) => {
-    const div = document.createElement('div');
-    div.classList.add('container');
-    const list = document.createElement('ul');
-    list.classList.add('list-group');
-    const listItem = document.createElement('li');
-    const head = document.createElement('h3');
-    const paragraph = document.createElement('p');
-    head.innerText = title.textContent;
-    paragraph.innerText = description.textContent;
-    listItem.append(head, paragraph);
-    list.append(listItem);
-    div.append(list);
-    body.append(div);
-  };
 
   const validateHandle = ({ target }) => {
     if (target.value === '') {
@@ -76,8 +46,8 @@ export default () => {
     axios.get(`https://cors-anywhere.herokuapp.com/${link}`, { crossdomain: true })
       .then(response => parser.parseFromString(response.data, 'application/xml'))
       .then((parsedXML) => {
-        makeFeedList(parsedXML.querySelector('title'), parsedXML.querySelector('description'));
-        makeItemsList(parsedXML.querySelectorAll('item'));
+        renderChanelList(parsedXML.querySelector('title'), parsedXML.querySelector('description'));
+        renderItemsList(parsedXML.querySelectorAll('item'));
       })
       .catch(console.log);
   };
